2cd333081a391826d0476c64afedb8ec6d7c4608
KSilhouette <prestongregory125@gmail.com>
2023-05-25 21:24:36 -0700

    maintenance

---
+++ Assets/ItemCollector.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollector : MonoBehaviour
+{
+    [SerializeField]
+    public bool PickUpItems = true;
+    [SerializeField]
+    public float PickUpRange = 1.0f;    
+
+    private Inventory _inventory;
+
+    void Start()
+    {
+        _inventory = GetComponent<Inventory>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (PickUpItems) Collect();
+    }
+
+    private void OnDrawGizmos()
+    {
+        UnityEditor.Handles.color = Color.green;
+        UnityEditor.Handles.DrawWireDisc(transform.position, transform.forward, PickUpRange);
+    }
+
+    private void Collect()
+    {
+        foreach (Collider2D collider in Physics2D.OverlapCircleAll(transform.transform.position, PickUpRange, LayerMask.GetMask("Item")))
+        {
+            collider.GetComponent<ItemEntity>()?.
+        }
+    }
+}
+
